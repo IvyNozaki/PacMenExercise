ea497474ba81b48cb2c30824670dac4a0c2e8bb1
Ivy Nozaki <ivyacasio@gmail.com>
2021-04-27 00:47:42 -1000

    file restructured

--- pacmen.js
+++ pacmen.js
@@ -18,12 +18,12 @@ const snack = [];
 
 const pacArray = [
   [
-    "PacMan1.png", 
-    "PacMan2.png"
+    "/images/PacMan1.png", 
+    "/images/PacMan2.png"
   ],
   [
-    "PacMan3.png", 
-    "PacMan4.png"
+    "/images/PacMan3.png", 
+    "/images/PacMan4.png"
   ]
 ];
 
